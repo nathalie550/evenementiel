f315fbd47e1f20f3fe1433d40a0a8dda11a55b62
Nathalie <jeannathalie550@gmail.com>
2024-04-01 19:01:23 +0200

    ajout du header modifié avec bannière photo et le titre rajouté

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The header for astra-enfant thè.
+ * The header for astra-enfant thème.
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
