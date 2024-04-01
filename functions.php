3ea881f25a1ae0d4bc21288fa3e8280153989bf4
Nathalie <jeannathalie550@gmail.com>
2024-04-01 19:03:18 +0200

    ajout du fichier functions.php maj

--- functions.php
+++ functions.php
@@ -8,11 +8,28 @@ function theme_enqueue_styles()
 {
 	// Enqueue parent style
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-	wp_enqueue_style('astra-enfant-style', get_stylesheet_directory_uri() . '/css/style.css', array('parent-style'), filemtime(get_stylesheet_directory() . '/css/style.css'));
+	wp_enqueue_style('astra-enfant-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), filemtime(get_stylesheet_directory() . '/style.css'));
 	wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/scripts/script.js', array('jquery'), false, true);
 }
 
+// ENREGISTRER LES EMPLACEMENTS DE MENU
+function theme_register_menus()
+{
+	register_nav_menus(
+		array(
+			'theme_location' => 'Menu principal',
+			'header' => 'Entête enfant astra',
+		)
+	);
 
+
+	add_theme_support('post-thumbnails'); // Exemple d'ajout de support pour les images à la une
+	add_theme_support('custom-logo');
+	add_theme_support('menus');
+}
+
+// Attachez la fonction à l'action after_setup_theme
+add_action('after_setup_theme', 'theme_register_menus');
 
 
 // Get customizer options form parent theme
@@ -25,3 +42,5 @@ if (get_stylesheet() !== get_template()) {
 		return get_option('theme_mods_' . get_template(), $default);
 	});
 }
+
+//PHOTOS
